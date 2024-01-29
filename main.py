0d11055984dbdef06f3194d160eef49344abb177
Beck LaBash <becklabash@gmail.com>
2024-01-29 15:16:32 -0500

    README

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ client = chromadb.PersistentClient(path=CHROMA_PERSIST_DIR)
 collection = client.get_or_create_collection(name = CHROMA_COLLECTION)
 
 while True:
-    query = input(": ")
+    query = input("> ")
     response = collection.query(query_texts=[query], n_results=k)
     docs = response['documents'][0]
     for doc in docs:
         print(doc)
-        print('---------------------------')
+        print('-' * 30)
