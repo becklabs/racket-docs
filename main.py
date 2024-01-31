d9011aca6d360212e4391d902591f3987fad8fb4
seb-sep <59621473+seb-sep@users.noreply.github.com>
2024-01-31 15:39:00 -0500

    optional rag, update docs

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import chromadb
 from rag import rag
+import sys
+
+use_rag = '-r' in sys.argv or '--rag' in sys.argv
 
 CHROMA_PERSIST_DIR = 'chromadb/'
 CHROMA_COLLECTION = 'racket'
-k = 10
+k = 5
 
 
 client = chromadb.PersistentClient(path=CHROMA_PERSIST_DIR)
@@ -13,9 +16,10 @@ while True:
     query = input("> ")
     response = collection.query(query_texts=[query], n_results=k)
 
-    print(rag(prompt=query, result=response))
+    if use_rag:
+        print(rag(prompt=query, result=response))
+    else:
+        docs = response['documents'][0]
+        for doc in docs:
+            print(doc)
     print('-' * 30)
-    # docs = response['documents'][0]
-    # for doc in docs:
-    #     print(doc)
-    #     print('-' * 30)
